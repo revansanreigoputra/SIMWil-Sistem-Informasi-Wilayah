2fa8f906ae8e751f8755d92e8d76adb12694aa8e
vidoonoth <alfinvidoo@gmail.com>
2025-08-29 10:28:37 +0700

    perbaikan migration desa & kepala desa

--- database/migrations/2025_08_29_100433_create_desas_table.php
+++ database/migrations/2025_08_29_100433_create_desas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,23 @@ return new class extends Migration
     {
         Schema::create('desas', function (Blueprint $table) {
             $table->id();
+            $table->string('kode_desa', 20)->unique();
+            $table->string('nama_desa', 100);
+            $table->text('alamat_kantor')->nullable();
+            $table->foreignId('kepala_desa_id')
+                ->nullable()
+                ->constrained('kepala_desa')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('email', 100)->nullable();
+            $table->string('telepon', 20)->nullable();
+            $table->string('website', 150)->nullable();
+            $table->string('logo', 255)->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
@@ -22,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('desas');
+        Schema::dropIfExists('desa');
     }
 };
--- database/migrations/2025_08_29_100434_create_kepala_desas_table.php
+++ database/migrations/2025_08_29_100434_create_kepala_desas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,17 @@ return new class extends Migration
     {
         Schema::create('kepala_desas', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_kepala_desa', 100);
+            $table->date('tanggal_lahir')->nullable();
+            $table->enum('jenis_kelamin', ['L', 'P']); // L = Laki-laki, P = Perempuan
+            $table->string('golongan_darah', 3)->nullable();
+            $table->string('kontak', 20)->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('masa_jabatan', 100)->nullable();
+            $table->string('nama_istri', 100)->nullable();
+            $table->integer('jumlah_anak')->default(0);
+            $table->text('sambutan')->nullable();
+            $table->string('foto', 255)->nullable(); // path ke foto
             $table->timestamps();
         });
     }
@@ -22,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kepala_desas');
+        Schema::dropIfExists('kepala_desa');
     }
 };
