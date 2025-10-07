703e0a361883b48b34f43bb1d75f3fae9b0ed00d
windawidiawati <windawidiawati281@gmail.com>
2025-10-07 11:34:39 +0700

    detailllll

---
+++ app/Http/Controllers/HasilpembangunanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\hasilpembangunan;
+use Illuminate\Http\Request;
+
+class HasilpembangunanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(hasilpembangunan $hasilpembangunan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(hasilpembangunan $hasilpembangunan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, hasilpembangunan $hasilpembangunan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(hasilpembangunan $hasilpembangunan)
+    {
+        //
+    }
+}
